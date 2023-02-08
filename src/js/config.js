85025af81a3298da435357e90f39c5700ff1d4d2
nomeroooo <nomermendoza10.10@gmail.com>
2023-02-09 00:57:35 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2667b352-da2a-436d-a3b2-e0b3d85474f4';
+export const MODAL_CLOSE_SEC = 2.5;
